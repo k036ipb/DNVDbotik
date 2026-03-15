0dba3b7674e937723d57b9a3b1b67550a8149706
k036ipb <k036ipb@gmail.com>
2026-03-15 19:44:08 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -48,9 +48,9 @@ def load_data():
     return data
 
 async def save_data(data):
-async with db_lock:
-with open(DATA_FILE, "w", encoding="utf-8") as f:
-json.dump(data, f, ensure_ascii=False, indent=2)
+    async with db_lock:
+        with open(DATA_FILE, "w", encoding="utf-8") as f:
+            json.dump(data, f, ensure_ascii=False, indent=2)
 
 # =========================
 
